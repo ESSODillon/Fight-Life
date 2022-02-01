a267d56e7bbcb8bb93356ad3c6cbc273988d1b65
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-02-01 15:32:29 -0500

    Sidebar component

--- src/App.js
+++ src/App.js
@@ -10,11 +10,13 @@ import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Event from "./pages/event/Event";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Sidebar />
         <div className="container">
           <Navbar />
           <Switch>
