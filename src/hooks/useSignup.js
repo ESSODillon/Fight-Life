dd10d7abc107dcdc141901e9d37a55ea976e7eea
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-02-02 11:12:31 -0500

    Signing a User Up

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -23,12 +23,12 @@ export const useSignup = () => {
         throw new Error("Could not complete signup");
       }
 
-      // Upload user thumbnail
+      // upload user thumbnail
       const uploadPath = `thumbnails/${res.user.uid}/${thumbnail.name}`;
       const img = await projectStorage.ref(uploadPath).put(thumbnail);
       const imgUrl = await img.ref.getDownloadURL();
 
-      // add display name to user
+      // add display AND PHOTO_URL name to user
       await res.user.updateProfile({ displayName, photoURL: imgUrl });
 
       // dispatch login action
