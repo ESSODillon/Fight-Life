a0337263dff9aeebc5058db06109051532facbb9
Dillon Polley <65629239+ESSODillon@users.noreply.github.com>
2022-02-06 17:51:08 -0500

    Project details page & more error handling

--- src/pages/event/Event.js
+++ src/pages/event/Event.js
@@ -1,6 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useDocument } from "../../hooks/useDocument";
+
 // Styles
 import "./Event.css";
 
 export default function Event() {
-  return <div>Event Details</div>;
+  const { id } = useParams();
+  const { error, document } = useDocument("events", id);
+
+  if (error) {
+    return <div className="error">{error}</div>;
+  }
+
+  if (!document) {
+    return <div className="loading">Loading...</div>;
+  }
+
+  return (
+    <div className="event-details">
+      <h1>{document.eventName}</h1>
+    </div>
+  );
 }
